33a636eca3f2b1fb0039ab58ba6eea5546d30989
Kim Kyung Seo (김경서) <biosnow19@sookmyung.ac.kr>
2020-08-20 00:16:44 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+question.style.textAlign="center";
+
+var br = document.createElement("br");
+document.body.appendChild(br);
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -7,9 +12,10 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
+
 let questions = [
     {
-        question: '여유로운 주말, 어떤 취미생활을 즐기고 싶나요?',
+        question: '여유로운 주말, \n 무엇으로 시간을 보낼까요?',
         choice1: '베이킹',
         choice2: '일기쓰기',
         choice3: '소설책 읽기',
@@ -18,50 +24,49 @@ let questions = [
     },
     {
         question:
-            "편의점에서 어떤 간식을 사고 싶나요?",
+            "편의점에서 \n 어떤 간식을 사고 싶나요?",
         choice1: "사탕",
         choice2: "초콜릿",
         choice3: "말린 과일",
         choice4: "나쵸",
-        answer: 1,
+        answer: 2,
     },
     {
-        question: "인테리어에 도전! 어떤 물품을 살까요?",
+        question: "인테리어에 도전! \n 어떤 물품을 살까요?",
         choice1: "파스텔 컬러 벽지",
         choice2: "풀음향장비",
         choice3: "인테리어용 책",
         choice4: "수납용품",
-        answer: 1,
+        answer: 2,
     },
     {
-        question: '아침에 일어났을 때 날씨가 어떠면 좋을까요?',
+        question: '아침에 일어났을 때 \n 날씨가 어떠면 좋을까요?',
         choice1: '햇살이 비치는 날씨',
         choice2: '구름이 깔린 날씨',
         choice3: '비가 조용하게 내리는 날씨',
         choice4: '햇빛이 없는 시원한 날씨',
-        answer: 1,
+        answer: 3,
     },
     {
-        question: '창가에 둘 화분으로는 어떤 것이 좋을까요?',
+        question: '창가에 두는 화분으로는 \n 어떤 것이 좋을까요?',
         choice1: '허브',
         choice2: '선인장',
         choice3: '데이지',
         choice4: '스투키',
-        answer: 1,
+        answer: 2,
     },
     {
-        question: '친구와의 쇼핑에서 어떤 옷을 살까요?',
+        question: '친구와의 쇼핑에서 \n 어떤 옷을 살까요?',
         choice1: '밝은 색의 티셔츠',
         choice2: '와이드팬츠',
         choice3: '맨투맨',
         choice4: '펑키 프린팅 티셔츠',
-        answer: 1,
+        answer: 4,
     }
 ];
-
-//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 6;
+
 
 startGame = () => {
     questionCounter = 0;
